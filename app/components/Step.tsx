f1cd2a62dc981bd93ad94e6bc49f20bf914529d1
박세영 <psy0909@midasin.com>
2024-06-16 21:00:19 +0900

    feat: [react-hook-form handleSubmit] + [remix action]

--- app/components/Step.tsx
+++ app/components/Step.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 import { Item } from "../types/item.type";
 import Group from "./Group";
 import { useForm, FieldValues, type UseFormRegister } from "react-hook-form";
+import { Form, useSubmit } from "@remix-run/react";
 
 interface Props {
   item: Item;
@@ -10,24 +11,29 @@ export const RegisterContext =
   createContext<UseFormRegister<FieldValues> | null>(null);
 
 const Step = ({ item }: Props) => {
+  const submit = useSubmit();
   const { handleSubmit, register } = useForm();
 
   const onSubmit = (data: Object) => {
-    console.log(JSON.stringify(data, null, 2));
+    console.log("client");
+    console.log(data);
+
+    // @ts-ignore
+    submit(data, { method: "POST" });
   };
 
   return (
-    <div>
-      <h1>{item.title}</h1>
-      <RegisterContext.Provider value={register}>
-        {item.childrenItemList?.map((groupItem) => (
-          <Group key={groupItem.title} item={groupItem} />
-        ))}
-        <button type="submit" onClick={handleSubmit(onSubmit)}>
-          제출
-        </button>
-      </RegisterContext.Provider>
-    </div>
+    <RegisterContext.Provider value={register}>
+      <div>
+        <h1>{item.title}</h1>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          {item.childrenItemList?.map((groupItem) => (
+            <Group key={groupItem.title} item={groupItem} />
+          ))}
+          <button type="submit">제출</button>
+        </Form>
+      </div>
+    </RegisterContext.Provider>
   );
 };
 
--- app/routes/step.1.tsx
+++ app/routes/step.1.tsx
@@ -2,10 +2,19 @@ import { useLoaderData } from "@remix-run/react";
 import { Item } from "../types/item.type";
 import { step1data } from "../config/step1.config";
 import Step from "../components/Step";
+import { ActionFunctionArgs } from "@remix-run/node";
 
 const getData = async () => new Promise((res) => res(step1data));
 
 export const loader = async () => await getData();
+export const action = async ({ request }: ActionFunctionArgs) => {
+  console.log("server");
+
+  const formData = await request.formData();
+  console.log(formData);
+
+  return null;
+};
 
 const Step1 = () => {
   const item = useLoaderData<typeof loader>() as Item;
